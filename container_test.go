a5f5193680f8de3a5bc7a0bd63c15dd7a5757d03
Boris S.Bobejko <krokodilgandy@gmail.com>
2026-01-28 11:30:46 +0300

    add support hosted infras for test

--- container_test.go
+++ container_test.go
@@ -2,45 +2,10 @@ package gronats
 
 import (
 	"errors"
-	"testing"
 
 	"github.com/google/uuid"
-	natsClient "github.com/nats-io/nats.go"
-	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/require"
 )
 
 func UnexpectError() error {
 	return errors.New(uuid.NewString())
-}
-
-func TestNewContainer(t *testing.T) {
-	t.Run("should be able to fail", func(t *testing.T) {
-		t.Run("when can't get connection string", func(t *testing.T) {
-			exp := UnexpectError()
-
-			cont := NewMockNATsContainer(t)
-
-			cont.EXPECT().ConnectionString(mock.Anything).Return("", exp)
-
-			_, err := newContainer[Deps](t.Context(), cont, config{})
-			require.ErrorIs(t, err, exp)
-		})
-
-		t.Run("when construction nats client failed", func(t *testing.T) {
-			exp := UnexpectError()
-
-			cont := NewMockNATsContainer(t)
-
-			cont.EXPECT().ConnectionString(mock.Anything).Return("", nil)
-
-			_, err := newContainer[Deps](t.Context(), cont, config{
-				natsConnector: func(url string, options ...natsClient.Option) (*natsClient.Conn, error) {
-					return nil, exp
-				},
-			})
-			require.ErrorIs(t, err, exp)
-		})
-	})
-
 }
